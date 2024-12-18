1f339ba38e9243ac606961a2e4596708f2241fe9
talhahzahid <talhahzahid415@gmail.com>
2024-12-18 16:30:02 +0500

    models

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+import dotenv from "dotenv"
+dotenv.config()
 import express from "express"
 const app = express()
 const port = 8000
+import connectdb from './src/db/index.js'
 
 app.get('/', (req, res) => {
     res.send('Hello Talha')
 })
 
 
-app.listen(port, () => {
-    console.log("SERVER IS RUNNIG AT PORT", port);
-})
+
+connectdb()
+    .then(() => {
+        app.listen(port, () => {
+            console.log("SERVER IS RUNNIG AT PORT", port);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,5 +3,17 @@ import users from "../models/user.models.js";
 
 const signUp = async (req, res) => {
     const { fullname, email, password } = req.body;
-    if (!email) return res.status(400).json({ messaage: "email is rrequired" })
+    if (!email) return res.status(400).json({ messaage: "email is rrequired" });
+    if (!password) return res.status(400).json({ messaage: "password is rrequired" });
+    try {
+        const user = await users.findOne({ email })
+        if (user) return res.status(400).json({ message: "user already exits" })
+        await users.create({ fullname, email, password })
+        res.status(200).json({ messaage: "user register successfully" })
+    } catch (error) {
+        res.status(400).json({ messaage: "error occured" })
+        console.log(error);
+    }
 }
+
+export { signUp }
